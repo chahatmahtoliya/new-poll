82b99c9fbfce2ecb7585824932cf1897eb29b90f
chahatmahtoliya <32595916+chahatmahtoliya@users.noreply.github.com>
2025-04-10 15:55:47 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/new-poll/', // Add this
+});
+
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
